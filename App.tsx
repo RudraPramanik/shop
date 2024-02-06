d39153c8994bee998e6ccb53dc160b57d1661f3a
Rudra <rudrahimangshoo2020@gmail.com>
2024-02-06 19:56:30 +0600

    auth-setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { FIREBASE_AUTH } from "./FirebaseConfig";
+import Home from "./app/screens/Home";
+import Login from "./app/screens/Login";
+import { User, onAuthStateChanged } from "firebase/auth";
+import { UserContext } from "./app/context/UserContext";
+import Favourite from "./app/screens/Favourite";
+
+
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+const InsideStack = createNativeStackNavigator();
+
+
+//insideLayout is the protected route when user loggedin only can visit it
+function InsideLayout({  }) {
+
+  return (
+    <Drawer.Navigator
+      initialRouteName="Home"
+    >
+      <Drawer.Screen name="Home" component={Home}  />
+      <Drawer.Screen name="Favourite" component={Favourite} />
+    </Drawer.Navigator>
+ 
+  );
+}
 
 export default function App() {
+  const [user, setUser] = useState<User | null>(null);
+  useEffect(() => {
+    onAuthStateChanged(FIREBASE_AUTH, (user) => {
+      setUser(user);
+      console.log("user", user);
+    });
+  }, []);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <UserContext.Provider value={{ user, setUser}}>
+    <NavigationContainer>
+      {/* conditionally render auth route or protected route */}
+      <Stack.Navigator initialRouteName="Login">
+        {user ? (
+          <Stack.Screen
+            name="Assesment"
+            component={InsideLayout}
+            options={{ headerShown: false }}
+          />
+        ) : (
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ headerShown: true }}
+          />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+    </UserContext.Provider>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
